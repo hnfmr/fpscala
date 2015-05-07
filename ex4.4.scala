a0062c430ba48030a205342c10c001358c351229
Wei Qi <qiwei.uk@gmail.com>
2015-05-07 17:36:43 +0800

    ex4.4

--- ex4.4.scala
+++ ex4.4.scala
@@ -2,17 +2,24 @@ import fpscala.errorhandling._
 
 object Exercise4_4 extends App {
   def sequence[A](a: List[Option[A]]): Option[List[A]] = {
-    a.foldLeft(None)((x,
+    def helper(aa: List[Option[A]], acc: Option[List[A]]): Option[List[A]] =
+      aa match {
+        case Nil => acc
+        case ah::at => ah match {
+          case None => None
+          case Some(x) => helper(at, acc.flatMap(xs => Some(x::xs)))
+        }
+      }
+
+    if (a == Nil) None
+    else helper(a, Some(List[A]()))
   }
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    a flatMap (aa => b map (bb => f(aa, bb)))
-  }
+  val b = List(None, Some(10), Some(11), Some(9))
+  val bs = sequence(b)
+  println(bs)
 
-  val ao = Some(10)
-  val bo = Some(11)
-  println(map2(ao, bo)((a:Int, b:Int) => a + b))
-
-  val bx = None
-  println(map2(ao, bx)((a:Int, b:Int) => a + b))
+  val a = List(Some(1), Some(2))
+  val as = sequence(a)
+  println(as)
 }
